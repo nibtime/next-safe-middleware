f62120bb3ada2fd2d041fc26e20f9abcb5968d8f
nibtime <kf@new.cannabis-med.org>
2022-02-25 17:45:28 +0000

    feat(middleware): :sparkles: configuration of Reporting API with report-to header

--- packages/next-safe-middleware/src/middleware/index.ts
+++ packages/next-safe-middleware/src/middleware/index.ts
@@ -3,3 +3,4 @@ export * from './utils';
 export { default as chain } from './chain';
 export { default as provideHashesOrNonce } from './provideHashesOrNonce';
 export { default as nextSafe } from './nextSafe';
+export { default as reporting } from './reporting';
