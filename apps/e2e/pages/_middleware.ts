74463b54ef8d8774048dfaf4c3d298d6eb504011
nibtime <kf@new.cannabis-med.org>
2022-02-26 16:01:18 +0000

    test: add reporting config to e2e test app

--- apps/e2e/pages/_middleware.ts
+++ apps/e2e/pages/_middleware.ts
@@ -1,13 +1,18 @@
-import type { Middleware } from '@next-safe/middleware';
-import { chain, nextSafe, provideHashesOrNonce } from '@next-safe/middleware';
-const isDev = process.env.NODE_ENV === 'development';
+import type { Middleware } from "@next-safe/middleware";
+import {
+  chain,
+  nextSafe,
+  provideHashesOrNonce,
+  reporting
+} from "@next-safe/middleware";
+const isDev = process.env.NODE_ENV === "development";
 
 const geoBlockMiddleware: Middleware = (req, evt, res, next) => {
-  const BLOCKED_COUNTRY = 'GB';
-  const country = req.geo.country || 'US';
+  const BLOCKED_COUNTRY = "GB";
+  const country = req.geo.country || "US";
 
   if (country === BLOCKED_COUNTRY) {
-    const response = new Response('Blocked for legal reasons', { status: 451 });
+    const response = new Response("Blocked for legal reasons", { status: 451 });
     // returning response terminates the chain
     return response;
     // returning response with next would continue the chain with response as `res` param
@@ -22,17 +27,36 @@ const nextSafeWithStrictDynamic = nextSafe((req) => {
   // https://web.dev/strict-csp/#step-4:-add-fallbacks-to-support-safari-and-older-browsers
   const strictDynamic = !isDev
     ? {
-        'script-src': [`'strict-dynamic'`, 'https:', `'unsafe-inline'`],
+        "script-src": [`'strict-dynamic'`, "https:", `'unsafe-inline'`],
       }
     : {};
   return {
     isDev,
     contentSecurityPolicy: {
       ...strictDynamic,
-      'style-src': `'unsafe-inline'`
+      "style-src": `'unsafe-inline'`,
     },
     // customize as you need: https://trezy.gitbook.io/next-safe/usage/configuration
   };
 });
 
-export default chain(nextSafeWithStrictDynamic, provideHashesOrNonce);
+const reportingMiddleware = reporting({
+  csp: {
+    reportUri: "https://example.com/csp-report-uri",
+    reportSample: true
+  },
+  reportTo: {
+    max_age: 1800,
+    endpoints: [
+      {
+        url: "https://example.com/reporting-api",
+      },
+    ],
+  },
+});
+
+export default chain(
+  nextSafeWithStrictDynamic,
+  reportingMiddleware,
+  provideHashesOrNonce
+);
