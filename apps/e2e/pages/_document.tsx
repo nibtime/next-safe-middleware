7428ee12a782ea51694674f664bd14e9fe97265e
nibtime <kf@new.cannabis-med.org>
2022-02-27 16:30:08 +0000

    test: strictDynamic middleware with reporting endpoint in e2e app

--- apps/e2e/pages/_document.tsx
+++ apps/e2e/pages/_document.tsx
@@ -1,14 +1,15 @@
 import Document, {
   provideComponents,
-} from '@next-safe/middleware/dist/document';
-import { Html, Main } from 'next/document';
-import React from 'react';
-import { getCssText } from 'stitches.config'
+} from "@next-safe/middleware/dist/document";
+import { Html, Main } from "next/document";
+import React from "react";
+import { getCssText } from "stitches.config";
 
+const violationScript = `console.log('I will always be blocked with a strict CSP')`;
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     try {
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -21,7 +22,7 @@ export default class MyDocument extends Document {
             />
           </>
         ),
-      }
+      };
     } finally {
     }
   }
@@ -30,7 +31,9 @@ export default class MyDocument extends Document {
     const { Head, NextScript } = provideComponents(this.props);
     return (
       <Html>
-        <Head />
+        <Head>
+          <script dangerouslySetInnerHTML={{ __html: violationScript }} />
+        </Head>
         <body>
           <Main />
           <NextScript />
--- apps/e2e/pages/_middleware.ts
+++ apps/e2e/pages/_middleware.ts
@@ -1,62 +1,43 @@
-import type { Middleware } from "@next-safe/middleware";
 import {
   chain,
   nextSafe,
-  provideHashesOrNonce,
+  strictDynamic,
   reporting
 } from "@next-safe/middleware";
+
 const isDev = process.env.NODE_ENV === "development";
 
-const geoBlockMiddleware: Middleware = (req, evt, res, next) => {
-  const BLOCKED_COUNTRY = "GB";
-  const country = req.geo.country || "US";
-
-  if (country === BLOCKED_COUNTRY) {
-    const response = new Response("Blocked for legal reasons", { status: 451 });
-    // returning response terminates the chain
-    return response;
-    // returning response with next would continue the chain with response as `res` param
-    // return next ? next(response) : response;
-  }
-  // returning nothing continues the chain
-};
-
-const nextSafeWithStrictDynamic = nextSafe((req) => {
-  // don't use strict-dynamic on `next dev`
-  // Browser support of strict-dynamic: https://caniuse.com/?search=strict-dynamic
-  // https://web.dev/strict-csp/#step-4:-add-fallbacks-to-support-safari-and-older-browsers
-  const strictDynamic = !isDev
-    ? {
-        "script-src": [`'strict-dynamic'`, "https:", `'unsafe-inline'`],
-      }
-    : {};
+const nextSafeMiddleware = nextSafe((req, res) => {
   return {
     isDev,
     contentSecurityPolicy: {
-      ...strictDynamic,
       "style-src": `'unsafe-inline'`,
     },
     // customize as you need: https://trezy.gitbook.io/next-safe/usage/configuration
   };
 });
 
+const vercelUrl = process.env.VERCEL_URL
+const reportEndpoint = vercelUrl ? `https://${vercelUrl}/api/reporting` : ""
+
 const reportingMiddleware = reporting({
   csp: {
-    reportUri: "https://example.com/csp-report-uri",
+    reportUri: process.env.CSP_REPORT_URI || reportEndpoint || "https://example.com/csp-report-uri",
     reportSample: true
   },
   reportTo: {
     max_age: 1800,
     endpoints: [
       {
-        url: "https://example.com/reporting-api",
+        url: process.env.CSP_REPORT_TO || reportEndpoint || "https://example.com/reporting-api",
       },
     ],
   },
+
 });
 
 export default chain(
-  nextSafeWithStrictDynamic,
-  reportingMiddleware,
-  provideHashesOrNonce
+  nextSafeMiddleware,
+  strictDynamic(),
+  reportingMiddleware
 );
