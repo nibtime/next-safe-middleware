357aaff466ceb947b334074a993b224a90102777
nibtime <kf@new.cannabis-med.org>
2022-02-28 20:34:19 +0000

    feat(middleware): :art: uniform builder interface
    - unpackConfig: all configs are either a config object or a config initalizer that can read req and res
    - withDefaultConfig: construct configurable middleware with deep merged with a default config

--- packages/next-safe-middleware/src/middleware/utils.ts
+++ packages/next-safe-middleware/src/middleware/utils.ts
@@ -1,43 +1,54 @@
 import type { CSP } from "../types";
-import { CSP_HEADER, CSP_HEADER_REPORT_ONLY, CSP_NONCE_HEADER } from "../constants";
+import {
+  CSP_HEADER,
+  CSP_HEADER_REPORT_ONLY,
+  CSP_NONCE_HEADER,
+} from "../constants";
 import { fromCspContent, toCspContent } from "../utils";
 import { nanoid } from "nanoid";
 
-export const setCspHeader = (cspContent: string, res: Response, reportOnly?: boolean) => {
+export const setCspHeader = (
+  cspContent: string,
+  res: Response,
+  reportOnly?: boolean
+) => {
   const isReportOnly = reportOnly ?? !!res.headers.get(CSP_HEADER_REPORT_ONLY);
-  if(isReportOnly) {
-    res.headers.delete(CSP_HEADER)
-    res.headers.set(CSP_HEADER_REPORT_ONLY, cspContent)
+  if (isReportOnly) {
+    res.headers.delete(CSP_HEADER);
+    res.headers.set(CSP_HEADER_REPORT_ONLY, cspContent);
+  } else {
+    res.headers.delete(CSP_HEADER_REPORT_ONLY);
+    res.headers.set(CSP_HEADER, cspContent);
   }
-  else {
-    res.headers.delete(CSP_HEADER_REPORT_ONLY)
-    res.headers.set(CSP_HEADER, cspContent)
-  }
-}
+};
 
 export const getCspHeader = (res: Response) => {
-  return res.headers.get(CSP_HEADER) || res.headers.get(CSP_HEADER_REPORT_ONLY)
-}
+  return res.headers.get(CSP_HEADER) || res.headers.get(CSP_HEADER_REPORT_ONLY);
+};
 
 export const pullCspFromResponse: (res: Response) => CSP | undefined = (
   res
 ) => {
-  const cspContent = getCspHeader(res)
+  const cspContent = getCspHeader(res);
   if (cspContent) {
     return fromCspContent(cspContent);
   }
   return undefined;
 };
 
-export const pushCspToResponse = (csp: CSP, res: Response, reportOnly?: boolean) => {
-  setCspHeader(toCspContent(csp), res, reportOnly)
+export const pushCspToResponse = (
+  csp: CSP,
+  res: Response,
+  reportOnly?: boolean
+) => {
+  setCspHeader(toCspContent(csp), res, reportOnly);
 };
 
 export const cspNonce = (res: Response) => {
-  let nonce = res.headers.get(CSP_NONCE_HEADER)
-  if(!nonce) {
-    nonce = nanoid()
-    res.headers.set(CSP_NONCE_HEADER, nonce)
+  let nonce = res.headers.get(CSP_NONCE_HEADER);
+  if (!nonce) {
+    nonce = nanoid();
+    res.headers.set(CSP_NONCE_HEADER, nonce);
   }
-  return nonce
-}
+  return nonce;
+};
