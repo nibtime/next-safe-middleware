252df9d5312d8a40d6b083fcffa621698235f497
nibtime <kf@new.cannabis-med.org>
2022-03-04 19:20:28 +0000

    feat(middleware): :building_construction: extend object merge of withDefaultConfig
    allows specifying custom merge logic by reusing `mergeDeepWithKey` from ramda.

--- packages/next-safe-middleware/src/middleware/builder.ts
+++ packages/next-safe-middleware/src/middleware/builder.ts
@@ -1,10 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
-import { mergeDeepRight } from "ramda";
+import { mergeDeepWithKey } from "ramda";
 import {
   EnsuredMiddleware,
   Middleware,
   MiddlewareBuilder,
   MiddlewareConfig,
+  WithoutBoolUnions,
 } from "./types";
 
 export const unpackConfig = async <Config extends Record<string, unknown>>(
@@ -15,27 +16,69 @@ export const unpackConfig = async <Config extends Record<string, unknown>>(
   return typeof cfg === "function" ? cfg(req, res) : cfg;
 };
 
+type KeyMerger = (k: string, x: any, z: any) => any;
+
 export const mergeConfigs =
   <Config extends Record<string, unknown>>(
-    left: MiddlewareConfig<Config>,
-    right: MiddlewareConfig<Config>
-  ) =>
+    left: MiddlewareConfig<WithoutBoolUnions<Config>>,
+    right: MiddlewareConfig<Config>,
+    keyMerger: KeyMerger = (k, l, r) => r
+  ): MiddlewareConfig<WithoutBoolUnions<Config>> =>
   async (req: NextRequest, res: Response) => {
     const leftCfg = await unpackConfig(req, res, left);
     const rightCfg = await unpackConfig(req, res, right);
-    return mergeDeepRight(leftCfg, rightCfg) as Config;
+    return mergeDeepWithKey(keyMerger, leftCfg, rightCfg);
   };
+
+const isTrue = (x: any): x is true => typeof x === "boolean" && x;
+
+const isFalse = (x: any): x is false => typeof x === "boolean" && !x;
+
+const isNonBool = <T>(x: any): x is Exclude<T, boolean> =>
+  typeof x !== "boolean" && !!x;
+
+const defaultConfigMergers: KeyMerger[] = [
+  // overwrite true flags with default config
+  (k, l, r) => (isTrue(r) && isNonBool(l) ? l : undefined),
+  // nullify default config on false flags
+  (k, l, r) => (isFalse(r) && isNonBool(l) ? null : undefined),
+  // if all mergers returned undefined, choose right value
+  (k, l, r) => r,
+];
+
+const chainMergers =
+  (mergers: KeyMerger[]): KeyMerger =>
+  (k, l, r) =>
+    mergers.reduce(
+      (v, next) => (typeof v === "undefined" ? next(k, l, r) : v),
+      undefined as any
+    );
 
 export const withDefaultConfig =
   <Config extends Record<string, unknown>>(
     builder: MiddlewareBuilder<Config>,
-    defaultCfg: MiddlewareConfig<Config>
+    defaultCfg:
+      | WithoutBoolUnions<Config>
+      | ((cfg: Partial<Config>) => WithoutBoolUnions<Config>),
+    ...keyMergers: KeyMerger[]
   ) =>
-  (cfg?: MiddlewareConfig<Config>) => {
+  (cfg?: MiddlewareConfig<Config>): Middleware =>
+  async (req, evt, res, next) => {
     if (cfg) {
-      return builder(mergeConfigs(defaultCfg, cfg));
+      const unpackedCfg = await unpackConfig(req, res, cfg);
+      return builder(
+        mergeConfigs(
+          typeof defaultCfg === "function"
+            ? defaultCfg(unpackedCfg)
+            : defaultCfg,
+          unpackedCfg,
+          chainMergers([...keyMergers, ...defaultConfigMergers])
+        )
+      )(req, evt, res, next);
     } else {
-      return builder(defaultCfg);
+      return builder(
+        typeof defaultCfg === "function" ? defaultCfg({}) : defaultCfg
+      )(req, evt, res, next);
     }
   };
 
--- packages/next-safe-middleware/src/middleware/types.ts
+++ packages/next-safe-middleware/src/middleware/types.ts
@@ -28,6 +28,14 @@ export type MiddlewareConfig<Config extends Record<string, unknown>> =
   | Config
   | ConfigInitalizer<Config>;
 
+export type WithoutBoolUnions<T extends object> = {
+  [P in keyof T]: T[P] extends boolean
+    ? T[P]
+    : Exclude<T[P], boolean> extends Record<string, unknown>
+    ? WithoutBoolUnions<Exclude<T[P], boolean>>
+    : Exclude<T[P], boolean>;
+};
+
 export type MiddlewareBuilder<Config extends Record<string, unknown>> = (
-  cfg: MiddlewareConfig<Config>
+  cfg: MiddlewareConfig<WithoutBoolUnions<Config>>
 ) => Middleware;
