49fc8d061c0d4ffa7ef7ecdfc60d5e0353fd017c
nibtime <kf@new.cannabis-med.org>
2022-07-12 17:32:23 +0000

    fix(middleware): test for file extensions in `isPageRequest` matcher
    I think it's agreeable that any path that ends in a dot, is a file and not a page request.
    In general, this is safer and more versatile than excluding by base path prefixes
    
    feat: provide logical operators for `ChainMatcher`
    
    `matchNot`, `matchAnd`, `matchOr`
    
    feat: provide matcher for preview mode request (`isPreviewModeRequest`)

--- packages/next-safe-middleware/src/middleware/matchers.ts
+++ packages/next-safe-middleware/src/middleware/matchers.ts
@@ -1,6 +1,40 @@
-import type { NextRequest } from "next/server";
+import type { NextRequestPredicate } from "./types";
 
-export const isPageRequest = (req: NextRequest) => {
-  const excludedRegex = /^\/(?:_next|api|fonts?|assets?|images?|documents?)\//;
-  return !excludedRegex.test(req.nextUrl.pathname);
+export const matchNot =
+  (matcher: NextRequestPredicate): NextRequestPredicate =>
+  (req) =>
+    !matcher(req);
+
+export const matchAnd =
+  (...matchers: NextRequestPredicate[]): NextRequestPredicate =>
+  (req) => {
+    let matches = true;
+    for (const matcher of matchers) {
+      matches = matcher(req);
+      if (!matches) return matches;
+    }
+    return matches;
+  };
+
+export const matchOr =
+  (...matchers: NextRequestPredicate[]): NextRequestPredicate =>
+  (req) => {
+    let matches = false;
+    for (const matcher of matchers) {
+      matches = matcher(req);
+      if (matches) return matches;
+    }
+    return matches;
+  };
+
+export const isPageRequest: NextRequestPredicate = (req) => {
+  const isUnderscoreNext = /^\/_next\//;
+  const isFile = /\..*$/;
+  const { pathname } = req.nextUrl;
+  return !isUnderscoreNext.test(pathname) && !isFile.test(pathname);
 };
+
+export const isPreviewModeRequest: NextRequestPredicate = (req) =>
+  !!req.cookies.get("__next_preview_data");
+
+export const isLiveModePageRequest = matchAnd(matchNot(isPreviewModeRequest), isPageRequest)
